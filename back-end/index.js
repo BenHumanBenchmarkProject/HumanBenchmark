1753d0dca726112a31036a7fa7ba3a3463144a04
Ben <snyderben598@gmail.com>
2025-06-23 10:27:39 -0700

    [HumanBenchmark][Codebase] add initial files

---
+++ back-end/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
