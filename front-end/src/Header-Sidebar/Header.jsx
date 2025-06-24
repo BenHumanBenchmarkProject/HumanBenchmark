dc90d13df13c0bf714f40472eadf84e81db0d01e
Ben <snyderben598@gmail.com>
2025-06-23 19:48:58 -0700

    [HumanBenchmark][Header][Sidebar] move header and siderbar to own files

---
+++ front-end/src/Header-Sidebar/Header.jsx
@@ -0,0 +1,27 @@
+import "./Header.css"
+import React from 'react';
+import blankPfp from '../assets/blank-pfp.jpg';
+
+const Header = ({ isLoggedIn, setIsLoggedIn }) => {
+    return(
+        <header className="banner">
+        <div className="logo">HUMAN BENCHMARK (LOGO)</div>
+        {isLoggedIn ? (
+        <button onClick={() => setIsLoggedIn(false)}>
+            <div className="account">
+            <h3>Account</h3>
+            <img src={blankPfp} alt="profile picture" width="40px" />
+            </div>
+        </button>
+        ) : (
+        <div className="account">
+            <button>Log in</button>
+            <button>Sign up</button>
+        </div>
+        )}
+    </header>
+    )
+
+};
+
+export default Header;
--- front-end/src/HomePage/HomePage.jsx
+++ front-end/src/HomePage/HomePage.jsx
@@ -1,62 +1,27 @@
 import "./HomePage.css"
 import React from 'react'
+import { useState } from "react"
 import bodyOutline from '../assets/body-outline.jpg'
 import { Link } from "react-router"
+import Header from "../Header-Sidebar/Header"
+import Sidebar from "../Header-Sidebar/Sidebar"
 
 
 const HomePage = () =>{
-    return (
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
         <>
-          <header className="banner">
-            <div className="logo">HUMAN BENCHMARK (LOGO)</div>
-            <div className="account">Account</div>
-          </header>
+          <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
 
           <div className="container">
-            <aside className="sidebar">
-              <div className="level">
-                <h2>LEVEL 82</h2>
-                <div className="xp-bar">
-                  <div className="xp-fill" style={{ "--final-width": "50%" }}></div>
-                </div>
-                <div className="xp-labels">
-                  <span>300xp</span>
-                  <span>600xp</span>
-                </div>
-              </div>
-
-              <div className="stats">
-                <h3>STATS</h3>
-                <div className="stat">
-                  <span>Strength</span>
-                  <div className="bar">
-                    <div style={{ "--final-width": "50%" }}></div>
-                  </div>
-                  50
-                </div>
-                <div className="stat">
-                  <span>Knowledge</span>
-                  <div className="bar">
-                    <div style={{ "--final-width": "30%" }}></div>
-                  </div>
-                  30
-                </div>
-                <div className="stat">
-                  <span>Overall</span>
-                  <div className="bar">
-                    <div style={{ "--final-width": "40%" }}></div>
-                  </div>
-                  40
-                </div>
-              </div>
-            </aside>
+            <Sidebar />
 
             <main className="main-content">
               <div className="top-buttons">
                 <Link to={"/leaderboard"}><button>LEADER BOARD</button></Link>
                 <Link to ={"/build-workout"}><button>BUILD WORKOUT</button></Link>
                 <Link to={"/log-workout"}><button>LOG WORKOUT</button></Link>
-                
+
               </div>
 
               <section className="diagram-section">
