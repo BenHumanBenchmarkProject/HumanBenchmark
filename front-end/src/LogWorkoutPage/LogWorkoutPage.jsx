cc15df70d60129109498fb309a6379063d548c1d
Ben <snyderben598@gmail.com>
2025-06-27 11:27:34 -0700

    [HumanBenchmark][Frontend][NavigationButton] add navigation buttons to each page

--- front-end/src/LogWorkoutPage/LogWorkoutPage.jsx
+++ front-end/src/LogWorkoutPage/LogWorkoutPage.jsx
@@ -2,14 +2,13 @@
 import "./LogWorkoutPage.css";
 import { Link } from "react-router";
 import SignInModal from "../SignInModal/SignInModal";
+import NavigationButtons from "../NaviagtionButtons/NavigationButtons";
 
 const LogWorkoutPage = () => {
   return (
     <div>
       <h1>Log Workout</h1>
-      <Link to={"/"}>
-        <button>Home</button>
-      </Link>
+      <NavigationButtons/>
       <SignInModal />
     </div>
   );
---
+++ front-end/src/NaviagtionButtons/NavigationButtons.jsx
@@ -0,0 +1,22 @@
+import "./NavigationButtons.css";
+import { Link } from "react-router";
+const NavigationButtons = () => {
+  return (
+    <div className="top-buttons">
+      <Link to={"/"}>
+        <button>HOME</button>
+      </Link>
+      <Link to={"/leaderboard"}>
+        <button>LEADER BOARD</button>
+      </Link>
+      <Link to={"/build-workout"}>
+        <button>BUILD WORKOUT</button>
+      </Link>
+      <Link to={"/log-workout"}>
+        <button>LOG WORKOUT</button>
+      </Link>
+    </div>
+  );
+};
+
+export default NavigationButtons;
