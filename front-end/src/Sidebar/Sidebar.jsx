896a5ea84ba24173db5f5cbb12959190faf3243f
Ben <snyderben598@gmail.com>
2025-06-27 16:11:25 -0700

    [HumanBenchmark][Frontend][Sidebar] add closing div

--- front-end/src/Sidebar/Sidebar.jsx
+++ front-end/src/Sidebar/Sidebar.jsx
@@ -19,7 +19,6 @@ const Sidebar = () => {
     return user ? user.level * 100 : 0; // default to 0 if nobody logged in
   };
 
-
   const xpPercentage = () => {
     if (!user) return 0;
     return (user.xp / neededXP()) * 100;
@@ -32,34 +31,19 @@ const Sidebar = () => {
     return (averageMax / 100) * 100; // scale relative to 100
   };
 
-
-return(
-  <aside className="sidebar">
-    <div className="level">
-      <h2>{`Level ${user ? user.level : 0}`}</h2>
-      <div className="xp-bar">
-        <div className="xp-fill" style={{ "--final-width": `${xpPercentage()}%` }}></div>
-      </div>
-      <div className="xp-labels">
-        <span>{`${user ? user.xp : 0}xp`}</span>
-        <span>{`${neededXP()}xp`}</span>
-      </div>
-    </div>
-
-    <div className="stats">
-      <h3>STATS</h3>
-      <div className="stat">
-        <span>Strength</span>
-        <div className="bar">
-          <div style={{ "--final-width": `${calculateOverallStrength()}%` }}></div>
+  return (
+    <aside className="sidebar">
+      <div className="level">
+        <h2>{`Level ${user ? user.level : 0}`}</h2>
+        <div className="xp-bar">
+          <div
+            className="xp-fill"
+            style={{ "--final-width": `${xpPercentage()}%` }}
+          ></div>
         </div>
-        {calculateOverallStrength()}
-      </div>
-      <div className="stat">
-        <span>Knowledge</span>
-        <div className="bar">
-          <div style={{ "--final-width": "30%" }}></div>
-
+        <div className="xp-labels">
+          <span>{`${user ? user.xp : 0}xp`}</span>
+          <span>{`${neededXP()}xp`}</span>
         </div>
       </div>
 
@@ -79,20 +63,37 @@ return(
           <div className="bar">
             <div style={{ "--final-width": "30%" }}></div>
           </div>
-          30
         </div>
-        <div className="stat">
-          <span>Overall</span>
-          <div className="bar">
-            <div style={{ "--final-width": "40%" }}></div>
+
+        <div className="stats">
+          <h3>STATS</h3>
+          <div className="stat">
+            <span>Strength</span>
+            <div className="bar">
+              <div
+                style={{ "--final-width": `${calculateOverallStrength()}%` }}
+              ></div>
+            </div>
+            {calculateOverallStrength()}
           </div>
-          40
+          <div className="stat">
+            <span>Knowledge</span>
+            <div className="bar">
+              <div style={{ "--final-width": "30%" }}></div>
+            </div>
+            30
+          </div>
+          <div className="stat">
+            <span>Overall</span>
+            <div className="bar">
+              <div style={{ "--final-width": "40%" }}></div>
+            </div>
+            40
+          </div>
         </div>
       </div>
-
     </aside>
   );
-
 };
 
 export default Sidebar;
