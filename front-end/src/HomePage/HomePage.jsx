df8f35a3f5b00e17d6e94c23b2a1bb52e2a78494
Ben <snyderben598@gmail.com>
2025-07-10 15:07:48 -0700

    [HumanBenchmark][Frontend][HomePage] updated homepage wireframe

--- front-end/src/HomePage/HomePage.jsx
+++ front-end/src/HomePage/HomePage.jsx
@@ -1,47 +1,73 @@
 import "./HomePage.css";
-import React from "react";
-import { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import bodyOutline from "../assets/body-outline.jpg";
+import axios from "axios";
+import { BASE_URL } from "../constants";
+import UserContext from "../userContext";
 
 import { NavigationButtons } from "../constants";
 
 const HomePage = () => {
+  const { user } = useContext(UserContext);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [workouts, setWorkouts] = useState([]);
+  const [friends, setFriends] = useState([]);
+
+  const fetchIncompleteWorkouts = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}users/${user.id}/workouts`);
+      const incompleteWorkouts = response.data.filter(
+        (workout) => !workout.isComplete
+      );
+      setWorkouts(incompleteWorkouts);
+      return incompleteWorkouts;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const fetchFriends = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}users/${user.id}/friends`);
+      setFriends(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchIncompleteWorkouts();
+    fetchFriends();
+  }, []);
+
   return (
     <>
       <div className="container">
         <NavigationButtons className="navButtons" />
 
-        <section className="diagram-section">
-          <img src={bodyOutline} alt="Body Outline" className="body-img" />
-          <div className="xp-labels-column">
-            <div className="xp-label">
-              Brain
-              <br />
-              200xp
-            </div>
-            <div className="xp-label">
-              Torso
-              <br />
-              50xp
-            </div>
-            <div className="xp-label">
-              Arms
-              <br />
-              100xp
-            </div>
-            <div className="xp-label">
-              Legs
-              <br />
-              300xp
-            </div>
-            <div className="xp-label">
-              Stamina
-              <br />
-              500xp
-            </div>
+        <div className="central-content">
+          <div className="home-workouts-box">
+            <div className="home-workouts-header">Your Workouts</div>
+            {workouts.map((workout, index) => (
+              <div key={index} className="home-workout-item">
+                <span>{workout.name}</span>{" "}
+              </div>
+            ))}
           </div>
-        </section>
+
+          <div className="calendar">
+            <h1>Calendar Placeholder</h1>
+          </div>
+
+          <div className="home-friends-box">
+            <div className="home-friends-header">Your Friends</div>
+            {friends.map((friend, index) => (
+              <div key={index} className="home-friend-item">
+                <span>{friend.username}</span>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
